974f318269d5676f444324c06d4ac763e3e1b7ed
nobleidea <nobletimer@gmail.com>
2020-06-19 23:15:27 +0900

    test2

--- ios/tutorials/tutorial2/TicTacToe/Root/RootInteractor.swift
+++ ios/tutorials/tutorial2/TicTacToe/Root/RootInteractor.swift
@@ -19,6 +19,7 @@ import RxSwift
 
 protocol RootRouting: ViewableRouting {
     // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+    func routeToLoggedIn(withPlayer1Name player1Name: String, withPlayer2Name: String)
 }
 
 protocol RootPresentable: Presentable {
@@ -31,27 +32,29 @@ protocol RootListener: class {
 }
 
 final class RootInteractor: PresentableInteractor<RootPresentable>, RootInteractable, RootPresentableListener {
-
     weak var router: RootRouting?
-
+    
     weak var listener: RootListener?
-
+    
     // TODO: Add additional dependencies to constructor. Do not perform any logic
     // in constructor.
     override init(presenter: RootPresentable) {
         super.init(presenter: presenter)
         presenter.listener = self
     }
-
+    
     override func didBecomeActive() {
         super.didBecomeActive()
         // TODO: Implement business logic here.
     }
-
+    
     override func willResignActive() {
         super.willResignActive()
         // TODO: Pause any business logic.
     }
-
+    
     // MARK: - LoggedOutListener
+    func didLogin(withPlayer1Name player1Name: String, player2Name: String) {
+        router?.routeToLoggedIn(withPlayer1Name: player1Name, withPlayer2Name: player2Name)
+    }
 }
