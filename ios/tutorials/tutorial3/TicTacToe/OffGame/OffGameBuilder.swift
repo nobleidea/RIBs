6e98696ca4a842820b30623d566aa628239d39a4
nobleidea <nobletimer@gmail.com>
2020-06-20 18:00:29 +0900

    test2

--- ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameBuilder.swift
+++ ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameBuilder.swift
@@ -19,11 +19,21 @@ import RIBs
 protocol OffGameDependency: Dependency {
     // TODO: Declare the set of dependencies required by this RIB, but cannot be
     // created by this RIB.
+    var player1Name: String { get }
+    var player2Name: String { get }
 }
 
 final class OffGameComponent: Component<OffGameDependency> {
 
     // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+    fileprivate var player1Name: String {
+        return dependency.player1Name
+    }
+    
+    fileprivate var player2Name: String {
+        return dependency.player2Name
+    }
+    
 }
 
 // MARK: - Builder
